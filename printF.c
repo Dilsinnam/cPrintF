a7b9d0b40ea4c8e80fae6fd9d24cef22f6465019
Dilsin Amirtharajah <DilsinAmirtharajahUSA@gmail.com>
2023-09-14 14:55:07 -0400

    Formatted code

--- printF.c
+++ printF.c
@@ -1,10 +1,11 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main () {
+int main()
+{
     int shirtNumber = 15;
     printf("Hello\nWorld\n");
-    printf("My favourite %s is %d, but also is %f.", "number", shirtNumber, 3.14);/*%d to print out an integer, followed by said integer. %s is for text. %f is for a decimal.*/
+    printf("My favourite %s is %d, but also is %f.", "number", shirtNumber, 3.14); /*%d to print out an integer, followed by said integer. %s is for text. %f is for a decimal.*/
 
     return 0;
-}   
+}
